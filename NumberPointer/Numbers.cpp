fc839869fde48752488d73936ca6a5b733ac7050
baris <baris@BARIS_SOMEROGLU>
2024-04-27 15:58:36 +0300

    Add project files.

---
+++ NumberPointer/Numbers.cpp
@@ -0,0 +1,39 @@
+﻿/*
+This code writes number in an order (from 1 to entered number by user)
+
+Developer = Barış Someroğlu
+Date = 27.04
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Function prototype
+void WriteOnScreen(int*);
+
+int main()
+{
+	int Number;
+
+	cout << "Please enter a number = ";
+	cin >> Number;
+
+	int* ptrNumber = &Number; // Pointer declaration
+
+	cout << "\n";
+	
+	// Call function
+	WriteOnScreen(&Number);
+
+	return 0;
+}
+
+// Function definition
+void WriteOnScreen(int* ptrNumber)
+{
+	for (int i = 1; i <= *ptrNumber; i++)
+	{
+		cout << i << endl;
+	}
+}
